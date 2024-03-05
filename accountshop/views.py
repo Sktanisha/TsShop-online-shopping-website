de4d654d117c1e63a2caab6c5c5e453dce1d5bb8
Sk. Sanjida Tanisha <sanjidatanisha99@gmail.com>
2024-03-04 21:38:35 -0500

    ...

--- accountshop/views.py
+++ accountshop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
 
 def index(request):
     return render(request, 'index.html')
