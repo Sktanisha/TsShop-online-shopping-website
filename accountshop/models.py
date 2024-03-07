ce8cf4696e3f37b0806dc449369777ba5f8c0e90
Sk. Sanjida Tanisha <sanjidatanisha99@gmail.com>
2024-03-06 22:31:28 -0500

    ...

--- accountshop/models.py
+++ accountshop/models.py
@@ -15,3 +15,9 @@ class profile(models.Model):
     confpassword = models.CharField(max_length = 20)
     email=models.EmailField(max_length=30)
     contact = models.IntegerField(max_length=11)
+
+class item_test(models.Model):
+    name_item = models.CharField(max_length = 30)
+    price = models.FloatField()
+    availability = models.BooleanField()
+    picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
